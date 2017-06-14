829d62ce24153617032db8cf7b6d930652fcfece
Joerg Roth <info@leylines.ch>
2017-06-14 08:09:26 +0200

    add source

---
+++ source/naturapraxis/scripts/site.js
@@ -0,0 +1,102 @@
+/* html-sites */
+require('../html/index.html');
+
+/* Styles */
+import styles from '../scss/site.scss';
+
+/* ScrolllReveal */
+window.sr = ScrollReveal()
+window.sr.reveal('.sr-left', {
+  duration: 1000,
+  distance: '50px',
+  easing: 'ease',
+  origin: 'left',
+  rotate: { x: 0, y: 0, z: 0 },
+  scale: 0.8
+}, 500);
+
+window.sr.reveal('.sr-top', {
+  duration: 1000,
+  distance: '50px',
+  easing: 'ease',
+  origin: 'top',
+  rotate: { x: 0, y: 0, z: 0 },
+  scale: 0.8
+}, 500);
+
+window.sr.reveal('.sr-flip', {
+  duration: 1000,
+  distance: '0px',
+  easing: 'ease',
+  origin: 'left',
+  rotate: { x: 0, y: -180, z: 0 },
+  scale: 0.8
+}, 500);
+
+window.sr.reveal('.timeline-flip', {
+  duration: 1000,
+  distance: '0px',
+  easing: 'ease',
+  origin: 'left',
+  rotate: { x: 0, y: -180, z: 0 },
+  scale: 0.8
+}, 500);
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
+
+    // Closes responsive menu when a link is clicked
+    $('.navbar-collapse>ul>li>a').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    });
+
+})(jQuery); // End of use strict
+
+var myCenter=new google.maps.LatLng(47.370674,8.528620);
+function initialize() {
+  var mapProp = {
+    center:myCenter,
+    zoom:14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var marker=new google.maps.Marker({
+    position:myCenter,
+    title:'Naturapraxis'
+  });
+  marker.setMap(map);
+  google.maps.event.addListener(marker,'click',function() {
+    map.setZoom(17);
+    map.setCenter(marker.getPosition());
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
